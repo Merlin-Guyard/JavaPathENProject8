6f0ed79327c494507a20ffdf0e372645a0346787
Merlin Guyard <merlin.guyard@gmail.com>
2023-09-01 09:32:49 +0200

    Revert "made interfaces"
    This reverts commit f5fb1ed1f8828fa673c435c89b1cc3b3c48f77ee.

--- TourGuide/src/main/java/tourGuide/repository/UserRepository.java
+++ TourGuide/src/main/java/tourGuide/repository/UserRepository.java
@@ -1,9 +1,4 @@
 package tourGuide.repository;
 
-import org.springframework.data.repository.CrudRepository;
-import tourGuide.user.User;
-
-import java.util.UUID;
-
-public interface UserRepository extends CrudRepository<User, UUID> {
+public class UserRepository {
 }
